e3240c5f45d330a9310d4d2118189d0f13505f99
Kristina <petrovnina22@mail.ru>
2023-01-11 12:32:55 +0300

    Задача_2

--- Team_2/Program.cs
+++ Team_2/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+// a = 5 b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+Console.WriteLine("Введите число ");
+int number1=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число ");
+int number2=Convert.ToInt32(Console.ReadLine());
+
+int numbermax=0;
+int numbermin=0;
+
+if (number1<number2)
+{
+    numbermax=number2;
+    numbermin=number1;
+}
+else if (number1>number2)
+{
+    numbermax=number1;
+    numbermin=number2;
+}
+ else
+ {
+    Console.WriteLine("Числа равны");
+ }  
+Console.WriteLine ("Максимальное чисело:");
+Console.WriteLine (numbermax);
+
+Console.WriteLine ("Минимальное чисело:");
+Console.WriteLine (numbermin);
